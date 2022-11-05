2255a8dc5e368a3ddfd640d5a5cf4d891eeb2a09
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-11-05 11:27:54 +0300

    Added a new review to a product via the leave_review instance method

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
+
+    def leave_review(user, star_rating, comment)
+        Review.create(star_rating: star_rating, comment: comment, product_id: id, user_id: user.id)
+    end
 end
