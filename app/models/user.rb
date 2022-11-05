28037d787eab37c7c495d82f7257ee38692d17eb
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-11-05 14:30:38 +0300

    Done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
     has_many :reviews
     has_many :products, through: :reviews
+
+    def favorite_product
+        self.products.order('star_rating').last
+        ##self.products.maximum('star_rating') returns the highest rating only
+    end
+
+    def remove_reviews(product)
+        self.reviews.where(product: product).destroy_all
+    end
 end
