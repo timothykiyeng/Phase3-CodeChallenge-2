18f5526650ad018eabbcecb8d9a92d0c9ef2a143
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-11-05 11:13:24 +0300

    Added print_review instance method that outputs to the terminal a string

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ActiveRecord::Base
     belongs_to :user
     belongs_to :product
+
+    def print_review
+      "Review for #{product.name} by #{user.name}: #{star_rating}. #{comment}"
+    end
 end
