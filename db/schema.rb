09c3180bee9f610e011a12356bd7fe7aba8f5f3e
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-11-05 10:39:23 +0300

    Added my review model and my seed data to create Reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_11_05_063436) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "star_rating"
+    t.string "comment"
+    t.integer "product_id"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,18 @@ Product.destroy_all
 User.destroy_all
 
 puts "Creating users..."
-user1 = User.create(name: Faker::Name.name)
-user2 = User.create(name: Faker::Name.name)
-user3 = User.create(name: Faker::Name.name)
+user1 = User.create(name: 'John Smith')
+user2 = User.create(name: 'Timothy Kiyeng')
+user3 = User.create(name: 'Alex Jeremy')
+user4 = User.create(name: 'John Paul')
+user5 = User.create(name: 'Milly Muteshi')
 
 puts "Creating products..."
-product1 = Product.create(name: "Stapler", price: 10)
-product2 = Product.create(name: "Whiteboard", price: 15)
-product3 = Product.create(name: "Dry Erase Markers", price: 5)
-product4 = Product.create(name: "Ballpoint Pens", price: 2)
-product5 = Product.create(name: "Scotch Tape", price: 3)
+p1 = Product.create(name: "Stapler", price: 10)
+p2 = Product.create(name: "Whiteboard", price: 15)
+p3 = Product.create(name: "Dry Erase Markers", price: 5)
+p4 = Product.create(name: "Ballpoint Pens", price: 2)
+p5 = Product.create(name: "Scotch Tape", price: 3)
 
 puts "Creating reviews..."
 # *****************************************************************
@@ -22,5 +24,16 @@ puts "Creating reviews..."
 # * and a review belongs to an user.                              *
 # *****************************************************************
 # Create Reviews Here
+
+Review.create(star_rating: 8, comment: 'I love it', product_id: p1.id, user_id: user1.id)
+Review.create(star_rating: 8, comment: 'Gorgeous', product_id: p4.id, user_id: user2.id)
+Review.create(star_rating: 7, comment: 'I love it', product_id: p5.id, user_id: user1.id)
+Review.create(star_rating: 4, comment: 'Beautiful', product_id: p2.id, user_id: user5.id)
+Review.create(star_rating: 6, comment: 'The worse experience', product_id: p1.id, user_id: user2.id)
+Review.create(star_rating: 8, comment: 'experience has been great', product_id: p3.id, user_id: user1.id)
+Review.create(star_rating: 9, comment: '100% great', product_id: p1.id, user_id: user3.id)
+Review.create(star_rating: 2, comment: 'Legit products', product_id: p3.id, user_id: user4.id)
+Review.create(star_rating: 6, comment: 'I love it', product_id: p4.id, user_id: user3.id)
+Review.create(star_rating: 8, comment: 'I love it', product_id: p2.id, user_id: user5.id)
 
 puts "Seeding done!"
