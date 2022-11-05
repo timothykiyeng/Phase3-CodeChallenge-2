13ba5a6dd1b13b4320ec44825884c2b2478feb8e
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-11-05 12:29:29 +0300

    Added average_rating and print_all_reviews methods to my Product class

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,14 @@ class Product < ActiveRecord::Base
     def leave_review(user, star_rating, comment)
         Review.create(star_rating: star_rating, comment: comment, product_id: id, user_id: user.id)
     end
+
+    def print_all_reviews
+        self.reviews.each do |review|
+            review.print_review
+        end
+    end
+
+    def average_rating
+        self.reviews.average('star_rating').to_f
+    end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,6 @@ class Review < ActiveRecord::Base
     belongs_to :product
 
     def print_review
-      "Review for #{product.name} by #{user.name}: #{star_rating}. #{comment}"
+      puts "Review for #{product.name} by #{user.name}: #{star_rating}. #{comment}"
     end
 end
